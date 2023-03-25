d87adbde01ba227ea7147e827041b4140625138f
praveen <cbsrirangapraveen.2004@gmail.com>
2023-03-25 17:08:37 +0530

    reg done

--- myapp/models.py
+++ myapp/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class userdetails(models.Model):
-    mobile = models.CharField(max_length=10)
+    mobile = models.CharField(max_length=10,null=False,blank=False)
     address = models.TextField()
     subscription = models.SmallIntegerField(default=0)
     user = models.OneToOneField(User,null=True,on_delete=models.CASCADE)
