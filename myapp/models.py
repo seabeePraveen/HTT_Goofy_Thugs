8d82356b8f8401e1467ef35916e6312355968ec8
praveen <cbsrirangapraveen.2004@gmail.com>
2023-03-25 15:20:36 +0530

    user absraction done

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class userdetails(models.Model):
+    mobile = models.IntegerField(max_length=10)
+    address = models.TextField()
+    subscription = models.SmallIntegerField(default=0)
+    user = models.OneToOneField(User,null=True,on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return str(self.user.username)
--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User,auth
+from . models import userdetails
 from django.contrib.auth import authenticate,login,logout
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
@@ -36,6 +37,8 @@ def signup(request):
         email = request.POST.get('email')
         password = request.POST.get('password1')
         password2 = request.POST.get('password2')
+        mobile = request.POST.get('mobile')
+        address = request.POST.get('address')
         if password == password2:
             if User.objects.filter(email=email).exists():
                 messages.info(request,"Email id already exists")
@@ -44,6 +47,8 @@ def signup(request):
                 user = User.objects.create_user(username = username, email = email, password=password)
                 user.save()
                 login(request,user)
+                detailing = userdetails.objects.create(user=user,mobile=mobile,address=address)
+                detailing.save()
                 return redirect('loginpage')
         else:
             messages.error(request,"passwords are not matched!")
